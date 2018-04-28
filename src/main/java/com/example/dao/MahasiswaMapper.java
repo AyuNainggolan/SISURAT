a6b1d70ba675e8d8c0a45d66345b7ad4f716af00
esther sihotang <esther.sihotang@ui.ac.id>
2018-04-28 15:34:46 +0700

    tambahkan kerangka file by ester

---
+++ src/main/java/com/example/dao/MahasiswaMapper.java
@@ -0,0 +1,17 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.example.model.UserAccountModel;
+
+@Mapper
+public interface MahasiswaMapper {
+ 
+}
---
+++ src/main/java/com/example/service/PengajuanSuratServiceDatabase.java
@@ -0,0 +1,5 @@
+package com.example.service;
+
+public class PengajuanSuratServiceDatabase implements PengajuanSuratService{
+
+}
