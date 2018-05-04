d147539cd71400c97d27dec536aa66c459f7ca5c
AyuNainggolan <nainggolanayu@gmail.com>
2018-05-04 14:09:33 +0700

    add detail pengajuan surat and modify layout

--- src/main/java/com/example/dao/PengajuanSuratMapper.java
+++ src/main/java/com/example/dao/PengajuanSuratMapper.java
@@ -1,8 +1,15 @@
 package com.example.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.model.PengajuanSuratModel;
 
 @Mapper
-public class PengajuanSuratMapper {
+public interface PengajuanSuratMapper {
+	
+	@Select("SELECT * FROM pengajuan_surat WHERE id = #{id_pengajuan_surat}")
+    PengajuanSuratModel getDetailPengajuanSurat (@Param("id_pengajuan_surat") int id_pengajuan_surat);
 
 }
--- src/main/java/com/example/service/PengajuanSuratServiceDatabase.java
+++ src/main/java/com/example/service/PengajuanSuratServiceDatabase.java
@@ -1,5 +1,21 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.PengajuanSuratMapper;
+import com.example.model.PengajuanSuratModel;
+
+@Service
 public class PengajuanSuratServiceDatabase implements PengajuanSuratService{
+	
+	@Autowired
+    private PengajuanSuratMapper pengajuanSurat;
+	
+	@Override
+	public PengajuanSuratModel getDetailPengajuanSurat(int id_pengajuan_surat) {
+		
+		return pengajuanSurat.getDetailPengajuanSurat(id_pengajuan_surat);
+	}
 
 }
