9c82a322d94a549629bba1bdc40b9e4de958c3d1
windiany <windianysitorus@gmail.com>
2018-05-12 14:46:36 +0700

    Adding download v1

--- src/main/java/com/example/controller/FrontController.java
+++ src/main/java/com/example/controller/FrontController.java
@@ -1,19 +1,22 @@
 package com.example.controller;
 
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -22,22 +25,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.model.MahasiswaModel;
 import com.example.model.MataKuliahModel;
 import com.example.model.PegawaiModel;
 import com.example.model.PengajuanSuratModel;
-import com.example.model.UserAccountModel;
 import com.example.service.JenisSuratService;
 import com.example.service.MahasiswaService;
 import com.example.service.MataKuliahService;
 import com.example.service.PegawaiService;
 import com.example.service.PengajuanSuratService;
 import com.example.service.StatusSuratService;
-import com.example.service.PengajuanSuratService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -65,8 +64,6 @@ public class FrontController {
     
     @Autowired
     ServletContext context;
-    
-    
     
 	@RequestMapping("/")
 	public String index(Model model) {
@@ -180,5 +177,28 @@ public class FrontController {
 		
         return "listSurat";
     }
-
+	
+	@RequestMapping(value = "/pengajuan/download/{id_pengajuan_surat}", method = RequestMethod.GET)
+	public ResponseEntity<Resource> download(String param, @PathVariable(value = "id_pengajuan_surat") int id_pengajuan_surat) throws IOException {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String fileName = auth.getName() + "_"+ id_pengajuan_surat+".pdf";
+	
+		File downloadedFile = new File("D:\\"+fileName);
+		Path path = Paths.get(downloadedFile.getAbsolutePath());
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+fileName);
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        System.out.println("The length of the file is : "+downloadedFile.length());
+        
+	    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+	    
+	    return ResponseEntity.ok()
+	            .headers(headers)
+	            .contentLength(downloadedFile.length())
+	            .contentType(MediaType.parseMediaType("application/octet-stream"))
+	            .body(resource);
+	}
 }
